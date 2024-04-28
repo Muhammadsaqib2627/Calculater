a75cb87ef76edc6b6fa19dd1c02ccd7b92ae6994
Muhammad Saqib <145840524+Muhammadsaqib2627@users.noreply.github.com>
2024-04-28 20:42:35 +0500

    git add .

---
+++ script..js
@@ -0,0 +1,10 @@
+
+function getTheData(val) {
+    let formData = document.getElementById("display");
+    if (val === "=") {
+        let result = eval(formData.value);
+        formData.value = result;
+        return
+    }
+    formData.value += val;
+}
